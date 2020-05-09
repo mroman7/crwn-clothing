c54eae82019f5afc43d5465ba35b4c6400436e60
Roman <romanmuhammad7@gmail.com>
2020-05-10 00:30:15 +0500

    Homepage menu items are setup completely

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Directory from '../../compontents/directory/directory.component';
+import './homepage.style.scss';
+
+const HomePage = () => (
+    <div className="homepage">
+        <Directory />
+    </div>
+);
+
+export default HomePage;
