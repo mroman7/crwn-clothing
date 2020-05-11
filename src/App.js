83549948376ec33e6095429c0d074b6196985257
Roman <romanmuhammad7@gmail.com>
2020-05-11 11:08:02 +0500

    Navigation and react routers are updated and setupe

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 
+const HatHome = () => (
+  <div>
+    <h1>Hat's Homepage:  </h1>
+    
+  </div>
+);
+
 function App() {
   return (
     <div>
-      <HomePage />    
+      <Route exact path='/' component={HomePage} />
+      <Route exact path='/hats/' component={HatHome} />
     </div>
   );
 }
